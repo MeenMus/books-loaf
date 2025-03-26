4d9aaf45f88c76f97a83ab6edd1557a92da545e3
Meen Mus <thebroski9999@gmail.com>
2025-03-26 23:50:52 +0800

    mus push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel - Book Loaf</title>
+        <title>Laravel - Book Loaf - MINE MUS</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
