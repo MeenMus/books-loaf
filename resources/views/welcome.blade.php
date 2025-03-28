1d0278ea2cd0e926395e08c70cc50b9f81a7d874
Meen Mus <thebroski9999@gmail.com>
2025-03-28 21:53:29 +0800

    JRA-002

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Books Loaf TO CHANGE JIRA</title>
+        <title>Books Loaf TO CHANGE JIRA TEST 2</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
