99e9e30a7c0ce44456732cf436122249068ad337
Hakim <hakim.abdel47@gmail.com>
2025-04-29 22:32:26 +0800

    change sidebar name to admin sidebar, put ui into dashboard and sidebar

---
+++ resources/views/admin/sidebarAdmin.blade.php
@@ -0,0 +1,22 @@
+@include('layouts.header')<div class="fixed top-0 left-0 h-full w-64 bg-gray-800 text-white shadow-md z-40">
+    <div class="p-4 text-lg font-semibold bg-gray-900">
+        Admin Panel
+    </div>
+    <ul class="mt-4 space-y-2 px-4">
+        <li>
+            <a href="" class="block py-2 px-3 rounded hover:bg-gray-700">Dashboard</a>
+        </li>
+        <li>
+            <a href="" class="block py-2 px-3 rounded hover:bg-gray-700">Manage Books</a>
+        </li>
+        <li>
+            <a href="" class="block py-2 px-3 rounded hover:bg-gray-700">Orders</a>
+        </li>
+        <li>
+            <a href="" class="block py-2 px-3 rounded hover:bg-gray-700">Users</a>
+        </li>
+        <li>
+            <a href="" class="block py-2 px-3 rounded hover:bg-gray-700">Logout</a>
+        </li>
+    </ul>
+</div>
